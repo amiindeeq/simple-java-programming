3b56bed9a1b8164edb9e134abe114412051bb140
amiindeeq <amiindeeqxussen@gmail.com>
2023-03-29 08:40:11 +0300

    add .

---
+++ app.java
@@ -0,0 +1,15 @@
+package semester5;
+
+public class Lesson1 {
+ 
+ public static void main(String [] ags) {
+	
+	//System.out.println("Hello world !!!");
+	//System.out.println("Welcome our first program");
+	//System.out.println("Macasalaama");
+	 
+	 //kii hoheeyy ama lesson1 or basic
+	System.out.println("xudayfi");
+}
+
+}
---
+++ index.java
@@ -0,0 +1,19 @@
+package semester5;
+
+public class Lesson2 {
+	 
+	 public static void main(String [] ags) {
+		 
+		 int phoneNUmb ;
+		 phoneNUmb=4117825;
+		 byte temp = 22;
+		 String dayofTheWeek = "Monday";
+		 String studName;
+		 short idNUmb= 3065;
+		 System.out.println("Todays temperture Is :" + temp);
+		 System.out.println("Todays the Weeks is :" + dayofTheWeek);
+		 System.out.println("My Phone Is :" + phoneNUmb);
+	}
+}
+
+
